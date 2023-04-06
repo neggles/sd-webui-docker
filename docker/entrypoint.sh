63b1db4f8f2f063a332cce08a9f2b1651a302cbd
Andrew Powers-Holmes <aholmes@omnom.net>
2023-04-06 13:52:09 +1000

    move kohya datadir back so copying paths is easier, tweak docker build

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -115,6 +115,13 @@ cp -vrfTs "${config_dir}/scripts/" "${repo_root}/scripts/"
 # Set git config so it won't warn and confuse the webui
 git config --global pull.ff only
 
+# make sure CUDA libs etc. are in path
+if [[ ! -z ${CUDA_HOME:-} ]]; then
+    ln -s ${CUDA_HOME} /usr/local/cuda
+    export PATH="${CUDA_HOME}/bin:${PATH}"
+    export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
+fi
+
 # Run startup script if it exists
 if [ -f "${config_dir}/startup.sh" ]; then
     pushd "${repo_root}" > /dev/null
--- docker/entrypoint_kohya.sh
+++ docker/entrypoint_kohya.sh
@@ -52,8 +52,15 @@ for tgt_path in "${!path_map[@]}"; do
     echo " -> ${src_path} (directory)"
 done
 
-# Set git config so it won't warn and confuse the webui
+# Set git config so it won't warn
 git config --global pull.ff only
+
+# make sure CUDA libs etc. are in path
+if [[ ! -z ${CUDA_HOME:-} ]]; then
+    ln -s ${CUDA_HOME} /usr/local/cuda
+    export PATH="${CUDA_HOME}/bin:${PATH}"
+    export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}"
+fi
 
 # Run startup script if it exists
 if [ -f "${config_dir}/startup.sh" ]; then
