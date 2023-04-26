4da98794488c235e437639d915acaaf0d66fe3df
Andrew Powers-Holmes <aholmes@omnom.net>
2023-04-26 17:54:57 +1000

    webui: link LyCORIS folder to LoRA folder for ease of use, reformat gitignore

--- docker/entrypoint_webui.sh
+++ docker/entrypoint_webui.sh
@@ -22,7 +22,7 @@ path_map["${repo_root}/models/torch_deepdanbooru"]="${data_dir}/Deepdanbooru"
 path_map["${repo_root}/models/BLIP"]="${data_dir}/BLIP"
 path_map["${repo_root}/models/midas"]="${data_dir}/MiDaS"
 path_map["${repo_root}/models/Lora"]="${data_dir}/Lora"
-path_map["${repo_root}/models/LyCORIS"]="${data_dir}/LyCORIS"
+path_map["${repo_root}/models/LyCORIS"]="${data_dir}/Lora"
 path_map["${repo_root}/models/openpose"]="${data_dir}/openpose"
 
 # extra hack for CodeFormer
